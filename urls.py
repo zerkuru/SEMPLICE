140df859b9ee5e4091e05dca19ee83bf3ea12431
zerkuru <zikonnikova@gmail.com>
2022-04-11 16:16:30 +0200

    HW1

---
+++ urls.py
@@ -0,0 +1,21 @@
+from datetime import date
+from views import Index, About
+
+
+# front controller
+def secret_front(request):
+    request['date'] = date.today()
+
+
+def other_front(request):
+    request['key'] = 'key'
+
+
+fronts = [secret_front, other_front]
+
+routes = {
+    '/': Index(),
+    '/about/': About(),
+    '/contacts/': Contacts(),
+    '/courses/': Courses(),
+}
