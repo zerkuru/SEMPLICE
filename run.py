e86a9921b55ffc08f8acc18686e09c98a2e1b63d
zerkuru <zikonnikova@gmail.com>
2022-04-21 11:33:10 +0200

    HW3

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 from wsgiref.simple_server import make_server
-import simba_framework.main
-import simba_framework.templator
+from simba_framework.main import Framework
 from urls import routes, fronts
 
-
-application = simba_framework.main.Framework(routes, fronts)
+application = Framework(routes, fronts)
 
 with make_server('', 8080, application) as httpd:
-    print("Start 8080")
+    print("Запуск на порту 8080...")
     httpd.serve_forever()
+
--- simba_framework/main.py
+++ simba_framework/main.py
@@ -1,3 +1,6 @@
+from quopri import decodestring
+from requests import GetRequests, PostRequests
+
 
 class PageNotFound404:
     def __call__(self, request):
@@ -6,25 +9,58 @@ class PageNotFound404:
 
 class Framework:
 
+    """Класс Framework - основа фреймворка"""
+
     def __init__(self, routes_obj, fronts_obj):
         self.routes_lst = routes_obj
         self.fronts_lst = fronts_obj
 
     def __call__(self, environ, start_response):
+        # получаем адрес, по которому выполнен переход
         path = environ['PATH_INFO']
 
+        # добавление закрывающего слеша
         if not path.endswith('/'):
             path = f'{path}/'
 
+        request = {}
+        # Получаем все данные запроса
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = PostRequests().get_request_params(environ)
+            request['data'] = Framework.decode_value(data)
+            print(f'Нам пришёл post-запрос: {Framework.decode_value(data)}')
+        if method == 'GET':
+            request_params = GetRequests().get_request_params(environ)
+            request['request_params'] = Framework.decode_value(request_params)
+            print(f'Нам пришли GET-параметры:'
+                  f' {Framework.decode_value(request_params)}')
+
+        # находим нужный контроллер
+        # отработка паттерна page controller
         if path in self.routes_lst:
             view = self.routes_lst[path]
         else:
             view = PageNotFound404()
-        request = {}
 
+        # наполняем словарь request элементами
+        # этот словарь получат все контроллеры
+        # отработка паттерна front controller
         for front in self.fronts_lst:
             front(request)
-
+        # запуск контроллера с передачей объекта request
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
+            val_decode_str = decodestring(val).decode('UTF-8')
+            new_data[k] = val_decode_str
+        return new_data
+
